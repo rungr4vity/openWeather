7f3142565d21c697321f264cd69182cc6aa9fc7f
p4co <rungr4vity@gmail.com>
2025-04-27 18:54:13 -0600

    Sunday

--- app/src/main/java/il/pacolo/com/appweather/presentation/screens/GeneralScreen.kt
+++ app/src/main/java/il/pacolo/com/appweather/presentation/screens/GeneralScreen.kt
@@ -1,20 +1,43 @@
 package il.pacolo.com.appweather.presentation.screens
 
-import androidx.compose.material3.Text
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun GeneralScreen() {
 
-    Text(text = "General Screen")
+    val estado  = mutableStateOf("")
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)
+    ) {
+
+        TextField(
+            value = estado.value,
+            onValueChange = {},
+            
+        )
+    }
 
 }
 
 
 
-@Preview(showBackground = true)
+@Preview(showBackground = true,device = Devices.PIXEL_4)
 @Composable
 fun GeneralScreenPreview(){
     GeneralScreen()
